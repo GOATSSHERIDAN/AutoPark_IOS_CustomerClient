7caf31ec4f86a699a6332dbb33024d1337789ab8
cuilin127 <39138182+cuilin127@users.noreply.github.com>
2020-10-09 11:46:11 -0400

    week4 update(SMS verification)

--- AutoPark_GOATS_UserApplication/ViewControllers/ViewController.swift
+++ AutoPark_GOATS_UserApplication/ViewControllers/ViewController.swift
@@ -6,12 +6,14 @@
 //
 
 import UIKit
-
+import FirebaseDatabase
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let ref = Database.database().reference()
+        ref.child("Users/1/LicensePlate").setValue("CDJJCNM")
     }
 
     @IBAction func unwindToHomeViewController(sender : UIStoryboardSegue){
