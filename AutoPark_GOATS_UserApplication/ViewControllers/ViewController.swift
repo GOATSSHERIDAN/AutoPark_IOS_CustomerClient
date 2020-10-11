1c9700dce76efef941dad18b61f9933b47ffa714
cuilin127 <39138182+cuilin127@users.noreply.github.com>
2020-10-11 00:15:21 -0400

    ???

--- AutoPark_GOATS_UserApplication/ViewControllers/ViewController.swift
+++ AutoPark_GOATS_UserApplication/ViewControllers/ViewController.swift
@@ -7,7 +7,10 @@
 
 import UIKit
 <<<<<<< HEAD
+<<<<<<< HEAD
 import FirebaseFirestore
+=======
+>>>>>>> parent of 7caf31e... week4 update(SMS verification)
 =======
 >>>>>>> parent of 7caf31e... week4 update(SMS verification)
 
@@ -17,12 +20,15 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
 <<<<<<< HEAD
+<<<<<<< HEAD
        // let ref = Database.database().reference()
         
         //ref.("Users/dave1/firstName").setValue("CDJJCNM")
         let db = Firestore.firestore()
         db.collection("Users").document("dave1").setData(["lastName":"hahaha"])
         
+=======
+>>>>>>> parent of 7caf31e... week4 update(SMS verification)
 =======
 >>>>>>> parent of 7caf31e... week4 update(SMS verification)
     }
